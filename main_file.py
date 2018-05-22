a448aea658e95d6014e7ae4dd0ffe67fba12355a
Chris Williams <chrwill@sas.upenn.edu>
2018-05-22 16:53:33 -0400

    Adding checking for unpacked file in main_file.py and will call the appropriate file.

--- main_file.py
+++ main_file.py
@@ -6,18 +6,24 @@ import ICVL
 import MNIST
 import os
 
+from unpack_files import unpackFiles
+
 # MNIST 28x28
 # ICVL 60x80
 
-dataSetInfo = ICVL.dataInfo()
+dataSetInfo = MNIST.dataInfo()
+
+if not os.path.exists(os.path.join('Data', 'Training', '{}_Training.pkl'.format(dataSetInfo.name))):
+    unpackFiles(dataSetInfo.name)
 
 (x_train, a_train, x_test, a_test) = dataSetInfo.load()
+
 
 if not os.path.exists("Output/" + dataSetInfo.name):
     os.mkdir("Output/" + dataSetInfo.name)
 # second layer size, third layer size, encoded size, input size
 model_parameters = model_parameters(
-    256, 1, 96, x_train.shape[1], 64, 48, 34, 1024, a_train.shape[1], dataSetInfo)
+    256, 1, 512, x_train.shape[1], 64, 48, 34, 512, a_train.shape[1], dataSetInfo)
 
 # Create the model with the parameters
 shared_vae = shared_vae_class(model_parameters)
--- unpack_files.py
+++ unpack_files.py
@@ -7,28 +7,16 @@ import cPickle
 import shutil
 
 
-with gzip.open('Data/MNIST_Data/Training/MNIST_Training.pkl.gz', 'rb') as f:
-    file_content = f.read()
+def unpackFiles(name):
+    with gzip.open('Data/' + name + '_Data/Training/' + name + '_Training.pkl.gz', 'rb') as f:
+        file_content = f.read()
 
-with open('Data/MNIST_Data/Training/MNIST_Training.pkl', 'rb') as f_in:
-    f_in.write(file_content)
+    with open('Data/' + name + '_Data/Training/' + name + '_Training.pkl', 'wb') as f_in:
+        f_in.write(file_content)
 
+    with gzip.open('Data/' + name + '_Data/Testing/' + name + '_Testing.pkl.gz', 'rb') as f:
+        file_content = f.read()
 
-with gzip.open('Data/MNIST_Data/Testing/MNIST_Testing.pkl.gz', 'rb') as f:
-    file_content = f.read()
+    with open('Data/' + name + '_Data/Testing/' + name + '_Testing.pkl', 'wb') as f_in:
+        f_in.write(file_content)
 
-with open('Data/MNIST_Data/Testing/MNIST_Testing.pkl', 'rb') as f_in:
-    f_in.write(file_content)
-
-with gzip.open('Data/ICVL_Data/Training/ICVL_Training.pkl.gz', 'rb') as f:
-    file_content = f.read()
-
-with open('Data/ICVL_Data/Training/ICVL_Training.pkl', 'rb') as f_in:
-    f_in.write(file_content)
-
-
-with gzip.open('Data/ICVL_Data/Testing/ICVL_Testing.pkl.gz', 'rb') as f:
-    file_content = f.read()
-
-with open('Data/ICVL_Data/Testing/ICVL_Testing.pkl', 'rb') as f_in:
-    f_in.write(file_content)
